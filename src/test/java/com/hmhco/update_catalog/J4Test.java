918c82e0d1a3061daa1b26858999b5710cf547e3
Francislainy Campos <francislainy.campos@hmhco.com>
2020-05-31 16:17:30 +0100

    Have only Junit4 and Junit5 simple tests.

---
+++ src/test/java/com/hmhco/update_catalog/J4Test.java
@@ -0,0 +1,12 @@
+package com.hmhco.update_catalog;
+
+import org.junit.Test;
+
+public class J4Test {
+
+    @Test
+    public void testing() {
+        System.out.println("test4===============");
+    }
+
+}
---
+++ src/test/java/com/hmhco/update_catalog/J5Test.java
@@ -0,0 +1,12 @@
+package com.hmhco.update_catalog;
+
+import org.junit.jupiter.api.Test;
+
+public class J5Test {
+
+    @Test
+    public void testing() {
+        System.out.println("test5;;;;;;;;;");
+    }
+
+}
